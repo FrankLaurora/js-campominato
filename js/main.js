192f0c6c10b624e59055b11cef47f32e2122ff55
FrankLaurora <francesco.laurora9@gmail.com>
2021-09-02 13:01:33 +0200

    Add function to check the clicked box

--- js/main.js
+++ js/main.js
@@ -6,8 +6,6 @@ function battlefield(num) {
     }
 }
 
-battlefield(100);
-
 //genero 16 numeri casuali tra 1 e 100 e li aggiungo a un array che non può contenere duplicati
 var bombs = [];
 
@@ -25,10 +23,26 @@ function placeBombs(num)/*num è un numero compreso tra 1 e il numero di caselle
 //creo una variabile che a cui assegno il numero della cella cliccata
 var clickedBox = playground.addEventListener("click",
     function(clicked) {
-        return clickedBox = parseInt(clicked.target.innerHTML);
+        let checkedBoxNum = parseInt(clicked.target.innerHTML);
+        let checkedBox = clicked.target;
+        probe(bombs, checkedBoxNum, checkedBox);
     }
 );
 
 var safeBoxes = []; /* qui verranno aggiunti, senza ripetizioni, i numeri contenuti nelle caselle cliccate che non contenevano mine*/
 
-//genero una funzione che verifichi la presenza di clickedBox in bombs e aggiunga clickedBox a safeBoxes se non è presente. Se clickedBox è presente in bombs la funzione genera un alert e restituisce il punteggio (la lunghezza di safeBoxes)
+//genero una funzione che verifichi la presenza di checkedBoxNum in bombs e aggiunga checkedBoxNum a safeBoxes se non è presente. Se checkedBoxNum è presente in bombs la funzione genera un alert e restituisce il punteggio (la lunghezza di safeBoxes). Inoltre, cambio il colore alla casella indicata dall'argomento tag.
+function probe(arr, elem, tag) {
+    if(!arr.includes(elem)){
+        if(!safeBoxes.includes(elem)) {
+            tag.classList.add("safe");
+            return safeBoxes.push(elem);
+        }
+    } else {
+        tag.classList.add("unsafe");
+        alert("Hai perso! Hai totalizzato " + safeBoxes.length + " punti.");
+    }
+}
+
+battlefield(50);
+placeBombs(50);
